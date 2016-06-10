c85a5be6a3f7c9399f4c453684be47fb0061b335
Andrew Maddaford <ajmaddaford@gmail.com>
2016-06-10 10:20:38 +0100

    Add html transform

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import settings
 import json
+import html
 import logging
 import logging.handlers
 import requests
@@ -11,7 +12,7 @@ app.config['SDX_STORE_URL'] = settings.SDX_STORE_URL
 app.config['SDX_TRANSFORM_CS_URL'] = settings.SDX_TRANSFORM_CS_URL
 
 @app.route('/pck', methods=['GET'])
-def do_transform():
+def do_transform_pck():
     store_url = app.config['SDX_STORE_URL'] + "/response?surveyId=023"
     result = requests.get(store_url).json()
     stored_json = result['results'][0]['surveyResponse']
@@ -22,6 +23,16 @@ def do_transform():
         status=200, \
         mimetype="application/json")
     return(response)
+
+@app.route('/html', methods=['GET'])
+def do_transform_html():
+    store_url = app.config['SDX_STORE_URL'] + "/response?surveyId=023"
+    result = requests.get(store_url).json()
+    stored_json = result['results'][0]['surveyResponse']
+
+    transform_url = app.config['SDX_TRANSFORM_CS_URL'] + "/html"
+    transformed_data = requests.post(transform_url, json=stored_json)
+    return(transformed_data.text)
 
 if __name__ == '__main__':
     # Startup
