a93861b188f50fef5e8945f2f82a3f05463b829e
Andrew Maddaford <ajmaddaford@gmail.com>
2016-06-16 17:35:26 +0100

    Get survey from store and print to log

--- consumer.py
+++ consumer.py
@@ -3,14 +3,20 @@ import os
 import logging
 import sys
 import settings
+import requests
 
 logging.basicConfig(stream=sys.stdout, level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
 
 logging.debug("Starting survey-notification consumer..")
 
+def get_survey_from_store(mongoid):
+    store_url = settings.SDX_STORE_URL + "/responses/" + mongoid
+    result = requests.get(store_url).text
+    logging.debug(result)
+
 def on_message(channel, method_frame, header_frame, body):
     logging.debug( method_frame.delivery_tag )
-    logging.debug( body )
+    get_survey_from_store(body.decode("utf-8"))
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 connection = pika.BlockingConnection(pika.URLParameters(settings.RABBIT_URL))
