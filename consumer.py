09bce610781920a7108b341cb2bdbf1a77e59613
Ian Wootten <i.wootten@gmail.com>
2016-07-06 15:21:52 +0100

    Log errors

--- consumer.py
+++ consumer.py
@@ -33,43 +33,63 @@ def deliver_binary_to_ftp(ftp, filename, data):
     ftp.storbinary('STOR ' + filename, stream)
 
 
-def get_survey_from_store(mongoid):
+def process_document(mongoid):
     store_url = settings.SDX_STORE_URL + "/responses/" + mongoid
-    result = requests.get(store_url).json()
+
+    r = requests.get(store_url)
+
+    if r.status_code != 200:
+        #  Retrieval failed
+        logger.error("Store retrieval failed", document_id=mongoid, store_url=store_url)
+
+        return
+
+    result = r.json()
+
     stored_json = result['survey_response']
     metadata = stored_json['metadata']
 
     bound_logger = logger.bind(user_id=metadata['user_id'], ru_ref=metadata['ru_ref'])
 
     sequence_url = settings.SDX_SEQUENCE_URL + "/sequence"
-    result = requests.get(sequence_url).json()
+    r = requests.get(sequence_url)
+
+    if r.status_code != 200:
+        bound_logger.error("Sequence retrieval failed", request_url=sequence_url)
+        return
+
+    result = r.json()
     sequence_no = result['sequence_no']
 
     transform_url = "%s/common-software/%d" % (settings.SDX_TRANSFORM_CS_URL, sequence_no)
-    transformed_data = requests.post(transform_url, json=stored_json)
-    zip_contents = transformed_data.content
+    r = requests.post(transform_url, json=stored_json)
+
+    if r.status_code != 200:
+        bound_logger.error("Transform failed", request_url=transform_url, sequence_no=sequence_no)
+        return
+
+    zip_contents = r.content
 
     try:
         z = zipfile.ZipFile(io.BytesIO(zip_contents))
-        bound_logger.debug("Zip contents:")
-        bound_logger.debug(z.namelist())
+        bound_logger.debug("Unzipped contents", filelist=z.namelist())
         ftp = connect_to_ftp()
         for filename in z.namelist():
             if filename.endswith('/'):
                 continue
-            bound_logger.debug("Processing file from zip: " + filename)
+            bound_logger.debug("Processing file from zip", filename=filename)
             edc_file = z.open(filename)
             deliver_binary_to_ftp(ftp, filename, edc_file.read())
         ftp.quit()
 
-    except (RuntimeError, zipfile.BadZipfile):
-        bound_logger.debug("Bad zip file!")
+    except (RuntimeError, zipfile.BadZipfile) as e:
+        bound_logger.error("Bad zip file", exception=e)
         # TODO: Need to deal with exception
 
 
 def on_message(channel, method_frame, header_frame, body):
     logger.debug(method_frame.delivery_tag)
-    get_survey_from_store(body.decode("utf-8"))
+    process_document(body.decode("utf-8"))
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 connection = pika.BlockingConnection(pika.URLParameters(settings.RABBIT_URL))
