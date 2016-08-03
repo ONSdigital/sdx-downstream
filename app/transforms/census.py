3f8ce89a40f401e5bdda3e1bbf93e0adc787092b
Elliott Jenkins <elliott.jenkins@methods.co.uk>
2016-08-03 15:48:52 +0100

    integrate queue publisher

--- app/transforms/census.py
+++ app/transforms/census.py
@@ -1,7 +1,7 @@
-import pika
 from app import settings
 from app.helpers.request_helper import remote_call, response_ok
 from app.settings import logger
+from app.queue_publisher import QueuePublisher
 
 
 class Census(object):
@@ -18,23 +18,9 @@ class Census(object):
         return response.content
 
     def process(self):
-        content = self.transform()
-        if content is False:
+        survey_xml = self.transform()
+        if survey_xml is False:
             return False
 
-        try:
-            logger.debug("XML Queueing Start")
-            connection = pika.BlockingConnection(pika.URLParameters(settings.RABBIT_URL))
-            channel = connection.channel()
-            channel.queue_declare(queue=settings.RABBIT_QUEUE_TESTFORM)
-            channel.basic_publish(exchange='',
-                                  properties=pika.BasicProperties(content_type='application/xml'),
-                                  routing_key=settings.RABBIT_QUEUE_TESTFORM,
-                                  body=content)
-            logger.debug("XML Queuing Success")
-            connection.close()
-            return True
-
-        except:
-            logger.error("XML Queuing Failed")
-            return False
+        publisher = QueuePublisher(logger, settings.RABBIT_URLS, settings.RABBIT_QUEUE_TESTFORM)
+        return publisher.publish_message(survey_xml, 'application/xml')
