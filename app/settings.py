d8cb23d493e9b834d014dcfcaff8765443ffb370
AndrewTorrance <Andrew.Torrance@QA.com>
2017-11-17 07:52:42 +0000

    Initial cloud foundry changes (#83)
    Initial cloud foundry changes, changed cloudfoundry service urls and settings so as to validate environment variables , added more tests

--- app/settings.py
+++ app/settings.py
@@ -1,46 +1,74 @@
+import json
 import logging
 import os
+from structlog import wrap_logger
 
 
 LOGGING_LEVEL = logging.getLevelName(os.getenv("LOGGING_LEVEL", "DEBUG"))
 LOGGING_FORMAT = "%(asctime)s.%(msecs)06dZ|%(levelname)s: sdx-downstream: %(message)s"
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-APP_TMP = os.path.join(APP_ROOT, 'tmp')
+logger = wrap_logger(
+    logging.getLogger(__name__)
+)
 
-# Default to true, cast to boolean
-SDX_STORE_URL = os.getenv("SDX_STORE_URL", "http://sdx-store:5000")
-SDX_TRANSFORM_CS_URL = os.getenv("SDX_TRANSFORM_CS_URL", "http://sdx-transform-cs:5000")
-SDX_TRANSFORM_CORA_URL = os.getenv("SDX_TRANSFORM_CORA_URL", "http://sdx-transform-cora:5000")
-SDX_SEQUENCE_URL = os.getenv("SDX_SEQUENCE_URL", "http://sdx-sequence:5000")
 
-CORA_SURVEYS = ['144']
+def _get_value(key, default_value=None):
+    """Gets a value from an environment variable , will use default if present else raise a value Error
+    """
+    value = os.getenv(key, default_value)
+    if not value:
+        logger.error("No value set for {}".format(key))
+        raise ValueError()
+    return value
 
-FTP_HOST = os.getenv('FTP_HOST', 'pure-ftpd')
-FTP_USER = os.getenv('FTP_USER')
-FTP_PASS = os.getenv('FTP_PASS')
 
-FTP_FOLDER = '/'
-FTP_HEARTBEAT_FOLDER = '/heartbeat'
+def parse_vcap_services():
+    vcap_services = _get_value("VCAP_SERVICES")
+    parsed_vcap_services = json.loads(vcap_services)
+    rabbit_config = parsed_vcap_services.get('rabbitmq')
+    rabbit_url = rabbit_config[0].get('credentials').get('uri')
+    rabbit_url2 = rabbit_config[1].get('credentials').get('uri') if len(rabbit_config) > 1 else rabbit_url
+    return rabbit_url, rabbit_url2
 
+
+def parse_non_vcap_services():
+    rabbit_url = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
+        hostname=_get_value('RABBITMQ_HOST', 'rabbit'),
+        port=_get_value('RABBITMQ_PORT', 5672),
+        user=_get_value('RABBITMQ_DEFAULT_USER', 'rabbit'),
+        password=_get_value('RABBITMQ_DEFAULT_PASS', 'rabbit'),
+        vhost=_get_value('RABBITMQ_DEFAULT_VHOST', '%2f')
+    )
+
+    rabbit_url2 = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
+        hostname=_get_value('RABBITMQ_HOST2', 'rabbit'),
+        port=_get_value('RABBITMQ_PORT2', 5672),
+        user=_get_value('RABBITMQ_DEFAULT_USER', 'rabbit'),
+        password=_get_value('RABBITMQ_DEFAULT_PASS', 'rabbit'),
+        vhost=_get_value('RABBITMQ_DEFAULT_VHOST', '%2f')
+    )
+    return rabbit_url, rabbit_url2
+
+
+if os.getenv("CF_DEPLOYMENT", False):
+    RABBIT_URL, RABBIT_URL2 = parse_vcap_services()
+else:
+    RABBIT_URL, RABBIT_URL2 = parse_non_vcap_services()
+
+RABBIT_URLS = [RABBIT_URL, RABBIT_URL2]
 RABBIT_QUEUE = 'sdx-survey-notification-durable'
 RABBIT_EXCHANGE = 'message'
 RABBIT_QUARANTINE_QUEUE = os.getenv('RABBIT_QUARANTINE_QUEUE', 'sdx-downstream-quarantine')
 
-RABBIT_URL = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
-    hostname=os.getenv('RABBITMQ_HOST', 'rabbit'),
-    port=os.getenv('RABBITMQ_PORT', 5672),
-    user=os.getenv('RABBITMQ_DEFAULT_USER', 'rabbit'),
-    password=os.getenv('RABBITMQ_DEFAULT_PASS', 'rabbit'),
-    vhost=os.getenv('RABBITMQ_DEFAULT_VHOST', '%2f')
-)
+FTP_HOST = _get_value('FTP_HOST', 'pure-ftpd')
+FTP_USER = os.getenv('FTP_USER')
+FTP_PASS = os.getenv('FTP_PASS')
+FTP_FOLDER = '/'
+FTP_HEARTBEAT_FOLDER = '/heartbeat'
 
-RABBIT_URL2 = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
-    hostname=os.getenv('RABBITMQ_HOST2', 'rabbit'),
-    port=os.getenv('RABBITMQ_PORT2', 5672),
-    user=os.getenv('RABBITMQ_DEFAULT_USER', 'rabbit'),
-    password=os.getenv('RABBITMQ_DEFAULT_PASS', 'rabbit'),
-    vhost=os.getenv('RABBITMQ_DEFAULT_VHOST', '%2f')
-)
+SDX_STORE_URL = _get_value("SDX_STORE_URL", "http://sdx-store:5000")
+SDX_TRANSFORM_CS_URL = _get_value("SDX_TRANSFORM_CS_URL", "http://sdx-transform-cs:5000")
+SDX_TRANSFORM_CORA_URL = _get_value("SDX_TRANSFORM_CORA_URL", "http://sdx-transform-cora:5000")
+SDX_SEQUENCE_URL = _get_value("SDX_SEQUENCE_URL", "http://sdx-sequence:5000")
 
-RABBIT_URLS = [RABBIT_URL, RABBIT_URL2]
+CORA_SURVEYS = ['144']
